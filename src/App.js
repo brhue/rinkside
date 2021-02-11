5cd36544be2e7c3e1c9baea9ed0258b672d1b239
Bradley Donahue <donahue.bradley@gmail.com>
2021-02-11 09:47:13 -0500

    Add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Schedule from "./Schedule";
 
 function App() {
   return (
-    <div>
-      <header className="m-b-1">
-        <nav className="container d-flex align-items-baseline justify-content-between">
-          <h1>
-            <a href="/">Hockey Info</a>
-          </h1>
-        </nav>
-      </header>
-      <main>
-        <Schedule />
-      </main>
-      <footer className="text-center m-t-auto p-1">
-        <small> &copy; 2021 Bradley Donahue</small>
-      </footer>
-    </div>
+    <Router>
+      <div>
+        <header className="m-b-1">
+          <nav className="container d-flex align-items-baseline justify-content-between">
+            <h1>
+              <Link to="/">Hockey Info</Link>
+            </h1>
+          </nav>
+        </header>
+        <main>
+          <Switch>
+            <Route path="/">
+              <Schedule />
+            </Route>
+          </Switch>
+        </main>
+        <footer className="text-center m-t-auto p-1">
+          <small> &copy; 2021 Bradley Donahue</small>
+        </footer>
+      </div>
+    </Router>
   );
 }
 
