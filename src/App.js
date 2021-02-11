811f2a7ebb571ab6939248f5c762579d5602348e
Bradley Donahue <donahue.bradley@gmail.com>
2021-02-11 09:57:35 -0500

    Modify folder structure slightly

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Schedule from "./Schedule";
+import Schedule from "./components/Schedule";
 
 function App() {
   return (
