96450e6077b46876cba8b7ab1b080724d512f359
Bradley Donahue <donahue.bradley@gmail.com>
2021-02-24 23:20:44 -0500

    Update app.js styles

--- src/App.jsx
+++ src/App.jsx
@@ -7,44 +7,42 @@ import Home from "./views/Home";
 function App() {
   return (
     <Router>
-      <div>
-        <header className="m-b-1">
-          <nav className="container d-flex align-items-baseline justify-content-between p-1">
-            <h1>
-              <Link to="/">Hockey Info</Link>
-            </h1>
-            <ul>
-              {/* <li>
-                <Link to="/games">Games</Link>
-              </li> */}
-              <li>
-                <Link to="/standings">Standings</Link>
-              </li>
-            </ul>
-          </nav>
-        </header>
-        <main>
-          <div className="container">
-            <Switch>
-              <Route path="/game/:gameId">
-                <SingleGame />
-              </Route>
-              <Route path="/standings">
-                <Standings />
-              </Route>
-              {/* <Route path="/games">
+      <header className="p-4 border-b mb-4">
+        <nav className="container mx-auto flex items-center justify-between">
+          <h1 className="font-bold text-3xl">
+            <Link to="/">Hockey Info</Link>
+          </h1>
+          <ul>
+            {/* <li>
+                  <Link to="/games">Games</Link>
+                </li> */}
+            <li>
+              <Link className="hover:bg-gray-400" to="/standings">Standings</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main className="px-4">
+        <div className="container">
+          <Switch>
+            <Route path="/game/:gameId">
+              <SingleGame />
+            </Route>
+            <Route path="/standings">
+              <Standings />
+            </Route>
+            {/* <Route path="/games">
                 <Schedule />
               </Route> */}
-              <Route path="/">
-                <Schedule />
-              </Route>
-            </Switch>
-          </div>
-        </main>
-        <footer className="text-center m-t-auto p-1">
-          <small> &copy; 2021 Bradley Donahue</small>
-        </footer>
-      </div>
+            <Route path="/">
+              <Schedule />
+            </Route>
+          </Switch>
+        </div>
+      </main>
+      <footer className="p-4 text-center">
+        <small> &copy; 2021 Bradley Donahue</small>
+      </footer>
     </Router>
   );
 }
