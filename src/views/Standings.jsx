867feca08e83f392dfc01ecfb5fe5acef3432832
Bradley Donahue <donahue.bradley@gmail.com>
2021-02-28 16:54:09 -0500

    Refactor and add styling

--- src/views/Standings.jsx
+++ src/views/Standings.jsx
@@ -1,25 +1,25 @@
 import { useState, useEffect } from "react";
 
 export default function Standings(props) {
-  const [standingsData, setStandingsData] = useState({ teamRecords: [] });
+  const [standingsData, setStandingsData] = useState([]);
   useEffect(() => {
     async function fetchStandingsData() {
       const baseUrl = "https://statsapi.web.nhl.com/api/v1/";
       const standingsUrl = `${baseUrl}standings/byLeague`;
       const response = await fetch(standingsUrl);
       const data = await response.json();
-      setStandingsData(data.records[0]);
+      setStandingsData(data.records[0].teamRecords);
     }
     fetchStandingsData();
   }, []);
 
   function handleSort(sortKey, direction) {
-    const sortedArray = [...standingsData.teamRecords];
-    if (sortKey === "goals") {
+    const sortedArray = [...standingsData];
+    if (sortKey === "g") {
       sortedArray.sort((a, b) => {
         return (b.goalsScored - a.goalsScored) * direction;
       });
-    } else if (sortKey === "points") {
+    } else if (sortKey === "pts") {
       sortedArray.sort((a, b) => {
         return (b.points - a.points) * direction;
       });
@@ -30,80 +30,64 @@ export default function Standings(props) {
         return (bDiff - aDiff) * direction;
       });
     }
-    setStandingsData({ teamRecords: sortedArray });
+    setStandingsData(sortedArray);
   }
 
-  const { teamRecords } = standingsData;
+  const tableData = standingsData.map((team) => {
+    return {
+      team: team.team.name,
+      gp: team.gamesPlayed,
+      w: team.leagueRecord.wins,
+      l: team.leagueRecord.losses,
+      ot: team.leagueRecord.ot,
+      pts: team.points,
+      g: team.goalsScored,
+      ga: team.goalsAgainst,
+      diff: team.goalsScored - team.goalsAgainst,
+    };
+  });
+
+  if (tableData.length === 0) {
+    return <h1>Loading...</h1>;
+  }
 
   return (
-    <>
-      <table className="w-100">
-        <thead>
+    <div className="p-4 rounded-lg shadow-md bg-white overflow-x-scroll">
+      <table className="table-auto w-full whitespace-nowrap">
+        <thead className="text-left border-b">
           <tr>
-            <th>Team</th>
-            <th>
-              <abbr title="Games Played">GP</abbr>
-            </th>
-            <th>
-              <abbr title="Wins">W</abbr>
-            </th>
-            <th>
-              <abbr title="Losses">L</abbr>
-            </th>
-            <th>
-              <abbr title="Overtime Losses">OT</abbr>
-            </th>
-            <th
-              data-dir={1}
-              onClick={(e) => {
-                const direction = e.currentTarget.getAttribute("data-dir");
-                e.currentTarget.setAttribute("data-dir", -direction);
-                handleSort("points", direction);
-              }}
-            >
-              <abbr title="Points">PTS</abbr>
-            </th>
-            <th
-              data-dir={1}
-              onClick={(e) => {
-                const direction = e.currentTarget.getAttribute("data-dir");
-                e.currentTarget.setAttribute("data-dir", -direction);
-                handleSort("goals", direction);
-              }}
-            >
-              <abbr title="Goals Scored">G</abbr>
-            </th>
-            <th>
-              <abbr title="Goals Against">GA</abbr>
-            </th>
-            <th
-              data-dir={1}
-              onClick={(e) => {
-                const direction = e.currentTarget.getAttribute("data-dir");
-                e.currentTarget.setAttribute("data-dir", -direction);
-                handleSort("diff", direction);
-              }}
-            >
-              <abbr title="Goal Differential">DIFF</abbr>
-            </th>
+            {Object.keys(tableData[0]).map((key) => {
+              return (
+                <th
+                  className="p-1"
+                  key={key}
+                  data-dir={1}
+                  onClick={(e) => {
+                    const direction = e.currentTarget.getAttribute("data-dir");
+                    e.currentTarget.setAttribute("data-dir", -direction);
+                    handleSort(key, direction);
+                  }}
+                >
+                  {key.toUpperCase()}
+                </th>
+              );
+            })}
           </tr>
         </thead>
         <tbody>
-          {teamRecords.map((team) => (
-            <tr key={team.team.id}>
-              <th>{team.team.name}</th>
-              <td>{team.gamesPlayed}</td>
-              <td>{team.leagueRecord.wins}</td>
-              <td>{team.leagueRecord.losses}</td>
-              <td>{team.leagueRecord.ot}</td>
-              <td>{team.points}</td>
-              <td>{team.goalsScored}</td>
-              <td>{team.goalsAgainst}</td>
-              <td>{team.goalsScored - team.goalsAgainst}</td>
+          {tableData.map((team) => (
+            <tr key={team.team}>
+              {Object.values(team).map((val, i) => {
+                return (
+                  <td key={i} className="p-1">
+                    {val}
+                  </td>
+                );
+              })}
             </tr>
           ))}
         </tbody>
       </table>
-    </>
+    </div>
   );
 }
