84342664240159d6ad5b0b4bbf2f1656e53a3039
Bradley Donahue <donahue.bradley@gmail.com>
2021-02-28 13:42:31 -0500

    Fix up styling and refactor
    Moved desired stats to show into an array and removed the hardcoded
    jsx values. This should make it easier to adjust styling and what
    is to be shown.

--- src/components/PlayerTable.jsx
+++ src/components/PlayerTable.jsx
@@ -1,32 +1,81 @@
 export default function PlayerTable({ skaters, allPlayers, goalies }) {
+  // TODO: Is there a better way to do this?
+  const skaterStats = [
+    {
+      key: "goals",
+      display: "Goals",
+    },
+    {
+      key: "assists",
+      display: "Assists",
+    },
+    {
+      key: "plusMinus",
+      display: "+/-",
+    },
+    {
+      key: "shots",
+      display: "Shots",
+    },
+    {
+      key: "blocked",
+      display: "Blocks",
+    },
+    {
+      key: "hits",
+      display: "Hits",
+    },
+    {
+      key: "penaltyMinutes",
+      display: "PIM",
+    },
+    {
+      key: "timeOnIce",
+      display: "TOI",
+    },
+  ];
+  const goalieStats = [
+    {
+      key: "saves",
+      display: "Saves",
+    },
+    {
+      key: "shots",
+      display: "Shots",
+    },
+    {
+      key: "savePercentage",
+      display: "Save %",
+    },
+    {
+      key: "timeOnIce",
+      display: "TOI",
+    },
+  ];
   return (
     <>
       {skaters && (
-        <table className="w-100">
-          <thead>
+        <table className="table-auto w-full whitespace-nowrap">
+          <thead className="text-left border-b">
             <tr>
-              <th>Player</th>
-              <th>Position</th>
-              <th>Goals</th>
-              <th>Assists</th>
-              <th>+/-</th>
-              <th>Shots</th>
-              <th>PIM</th>
-              <th>TOI</th>
+              <th className="p-1">Player</th>
+              {skaterStats.map((stat) => (
+                <th key={stat.key} className="p-1">
+                  {stat.display}
+                </th>
+              ))}
             </tr>
           </thead>
           <tbody>
             {skaters.map((skater) => {
               return (
-                <tr key={skater}>
-                  <td>{allPlayers[`ID${skater}`].person.fullName}</td>
-                  <td>{allPlayers[`ID${skater}`].position.abbreviation}</td>
-                  <td>{allPlayers[`ID${skater}`].stats.skaterStats.goals}</td>
-                  <td>{allPlayers[`ID${skater}`].stats.skaterStats.assists}</td>
-                  <td>{allPlayers[`ID${skater}`].stats.skaterStats.plusMinus}</td>
-                  <td>{allPlayers[`ID${skater}`].stats.skaterStats.shots}</td>
-                  <td>{allPlayers[`ID${skater}`].stats.skaterStats.penaltyMinutes}</td>
-                  <td>{allPlayers[`ID${skater}`].stats.skaterStats.timeOnIce}</td>
+                <tr key={skater} className="hover:bg-gray-100 transition-colors">
+                  <td className="p-1">{allPlayers[`ID${skater}`].person.fullName}</td>
+                  {skaterStats.map((stat) => (
+                    <td key={stat.key} className="p-1">
+                      {allPlayers[`ID${skater}`].stats.skaterStats[stat.key]}
+                    </td>
+                  ))}
                 </tr>
               );
             })}
@@ -34,27 +83,39 @@ export default function PlayerTable({ skaters, allPlayers, goalies }) {
         </table>
       )}
       {goalies && (
-        <table className="w-100">
-          <thead>
+        <table className="table-auto w-full whitespace-nowrap">
+          <thead className="text-left border-b">
             <tr>
-              <th>Goalie</th>
-              <th>Saves</th>
-              <th>Shots</th>
-              <th>Save %</th>
-              <th>TOI</th>
+              <th className="p-1">Goalie</th>
+              {goalieStats.map((stat) => (
+                <th key={stat.key} className="p-1">
+                  {stat.display}
+                </th>
+              ))}
             </tr>
           </thead>
           <tbody>
             {goalies.map((goalie) => {
+              // Calculate this here because I swear sometimes it doesn't exist on the stats object.
               const savePercentage =
                 allPlayers[`ID${goalie}`].stats.goalieStats.saves / allPlayers[`ID${goalie}`].stats.goalieStats.shots;
               return (
-                <tr key={goalie}>
-                  <td>{allPlayers[`ID${goalie}`].person.fullName}</td>
-                  <td>{allPlayers[`ID${goalie}`].stats.goalieStats.saves}</td>
-                  <td>{allPlayers[`ID${goalie}`].stats.goalieStats.shots}</td>
-                  <td>{savePercentage ? savePercentage.toFixed(3) : "-"}</td>
-                  <td>{allPlayers[`ID${goalie}`].stats.goalieStats.timeOnIce}</td>
+                <tr key={goalie} className="hover:bg-gray-100 transition-colors">
+                  <td className="p-1">{allPlayers[`ID${goalie}`].person.fullName}</td>
+                  {goalieStats.map((stat) => {
+                    if (stat.key === "savePercentage") {
+                      return (
+                        <td key={stat.key} className="p-1">
+                          {savePercentage ? savePercentage.toFixed(3) : "-"}
+                        </td>
+                      );
+                    }
+                    return (
+                      <td key={stat.key} className="p-1">
+                        {allPlayers[`ID${goalie}`].stats.goalieStats[stat.key]}
+                      </td>
+                    );
+                  })}
                 </tr>
               );
             })}
