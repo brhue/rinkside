75cc8748e4afc1a7845bae7705304d45dafe39e6
Bradley Donahue <donahue.bradley@gmail.com>
2021-03-04 13:26:26 -0500

    Add custom animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,21 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: "media", // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "fade-in": {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+      },
+      animation: {
+        fade: "fade-in .35s ease-in forwards",
+      },
+    },
   },
   variants: {
     extend: {
-      translate: ['group-hover']
+      translate: ["group-hover"],
     },
   },
   plugins: [],
