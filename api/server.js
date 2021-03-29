826855d889ff450066044f560878e52149835ce6
Bradley Donahue <donahue.bradley@gmail.com>
2021-03-29 15:47:18 -0400

    Remove console logs and unused code

--- api/server.js
+++ api/server.js
@@ -7,13 +7,9 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 const apiUrl = "https://api.nhle.com/stats/rest/en/leaders/";
-const queryParams = "?cayenneExp=season=20202021%20and%20gameType=2";
 
 app.get("/api/leaders/:position/:metric", async (req, res) => {
-  console.log(req.query);
-  console.log(req.params);
   const url = `${apiUrl}${req.params.position}/${req.params.metric}?${qs.stringify(req.query)}`;
-  console.log(url);
   try {
     const resp = await axios.get(url);
     res.json(resp.data);
@@ -22,7 +18,6 @@ app.get("/api/leaders/:position/:metric", async (req, res) => {
       error: true,
       message: err,
     });
-    // next(err);
   }
 });
 
